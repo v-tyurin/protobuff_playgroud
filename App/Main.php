71afdf95eccb4078be7833480602fe7c8f4474ee
Null <neonwired@gmail.com>
2020-05-23 17:24:45 +0500

    just play around

--- App/Main.php
+++ App/Main.php
@@ -5,6 +5,7 @@ namespace App;
 
 
 
+use Protobuff\Front\Ticket;
 use Protobuff\Person;
 
 class Main
@@ -16,6 +17,16 @@ class Main
         $protoMsg->setAge(12);
         $protoMsg->setIsProfileVerified(false);
         $protoMsg->setSmallPicture('test');
+        $ticket = new Ticket(['phone'=>'123','name'=>'111']);
+
+        $protoMsg->setTickets([$ticket]);
+        $tickets = $protoMsg->getTickets();
+        foreach ($tickets as $ticket){
+            $aa = new Ticket();
+            $aa->mergeFrom($ticket); // GET DATA FROM MESSAGE
+        }
+        $protoMsg->setTickets([$ticket]);
+        $protoMsg->setTickets([$ticket]);
         var_dump($protoMsg->serializeToJsonString());
         var_dump($protoMsg->serializeToString());
     }
